8e03c3fa1a8b8f1edb4d14e53963f60fba254d88
Jakaria Khan <jakariakhan0292@gmail.com>
2025-03-16 20:06:39 +0600

    update

--- routes/web.php
+++ routes/web.php
@@ -42,8 +42,10 @@ Route::post('/opinion', [UserRegistrationController::class, 'opinionSubmit'])->n
 Route::post('/calculate-payment-and-participants', [UserRegistrationController::class, 'paymentAndParticipants'])->name('payment.participants');
 
 Route::get('/payment-summary', function () {
-    if(empty(session()->get('validate_data')['mobile_number']) || empty(session()->get('total_amount')) || session()->has('otp_verified')) {
+    if(empty(session()->get('validate_data')['mobile_number']) || empty(session()->get('total_amount'))) {
         return redirect()->route('home');
+    } else if(session()->has('otp_verified')){
+        return redirect()->route('payment');
     }
     return view('payment_summary');
 })->name('payment.summary');
